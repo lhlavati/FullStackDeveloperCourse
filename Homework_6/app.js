81523de956026db79bb0e01cf19fac86de1c0158
Luka Hlavati <luka.hlavati@enea.com>
2023-04-14 20:15:25 +0200

    added functionalty on calculate button

--- Homework_6/app.js
+++ Homework_6/app.js
@@ -1,14 +1,19 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, () => {
     console.log("Server is running!");
 })
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/index.html");
 })
 
 app.post("/", (req, res) => {
-    console.log("test");
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+    res.send(bmi);
 })
