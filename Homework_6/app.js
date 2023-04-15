0aff268d5616269d0649213205170f60c0f35ddd
Luka Hlavati <luka.hlavati@enea.com>
2023-04-15 11:45:41 +0200

    changed img src localy instead of url

--- Homework_6/app.js
+++ Homework_6/app.js
@@ -1,6 +1,7 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(3000, () => {
